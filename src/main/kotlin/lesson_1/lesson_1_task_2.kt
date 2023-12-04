5ecbee1bc8ee3707b24c59dd802aa782b4dabdd4
Rail_Minyaev <blastrm@gmail.com>
2023-12-04 18:17:29 +0500

    KS-1-2 one employee quit

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,15 @@
+package lesson_1
+
+fun main() {
+    var countOrders: UInt = 75U //Var так как в будущем количество заказов будут увеличиваться
+    val thanksForPurchase: String = "Спасибо за покупку, заказывайте у нас со скидками"
+
+    println(countOrders)
+    println(thanksForPurchase)
+
+    var countEmployee: UShort = 2000U
+  //  println(countEmployee)
+    countEmployee = 1999U
+
+    println(countEmployee)
+}

7587da3a6f3f5f912d9920aecc2983501dac0f7a
Rail_Minyaev <blastrm@gmail.com>
2023-12-06 00:45:39 +0500

    KS-1-5 Used String.format %02d Perevod sekund v chasy minuty i sekundi

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,21 +1,20 @@
 package lesson_1
 
-fun main()
-{
+fun main() {
+    val oneHourInSeconds: Short = 3600
+    val oneMinuteInSeconds: Short = 60
+    val secondsFlight: Short = 6480
 
-    val secondsFlight:Short=6480
-    val hours:Int=secondsFlight/3600
-    val minutes:Int=(secondsFlight-3600*hours)/60
-    val seconds:Int=secondsFlight%60
+    val hours: Int = secondsFlight / oneHourInSeconds
+    val minutes: Int = (secondsFlight - oneHourInSeconds * hours) / oneMinuteInSeconds
+    val seconds: Int = secondsFlight % oneMinuteInSeconds
 
-    var outHours:String="$hours" //Перевожу в стринг,чтоб потом можно было проверить на двузначность и дописать ноль, если число однозначное
-    var outminutes:String="$minutes"
-    var outseconds:String="$seconds"
+    val outHours: String = String.format("%02d", hours)
+    val outMinutes: String = String.format("%02d", minutes)
+    val outSeconds: String = String.format("%02d", seconds)
 
-    if(hours<=9)  outHours="0$hours" //Проверка на двузначность числа, если нет дописываем ноль
-    if(minutes<=9)  outminutes="0$minutes"
-    if(seconds<=9)  outseconds="0$seconds"
 
-    println("$outHours:$outminutes:$outseconds")
+    println("$outHours:$outMinutes:$outSeconds")
+
 }
 

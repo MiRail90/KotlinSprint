24c80cfbcb679b104eac3ee087b2ec615818fa3e
Rail_Minyaev <blastrm@gmail.com>
2023-12-06 16:51:01 +0500

    KS-2-1 Average sum

---
+++ src/main/kotlin/lesson_2/lesson2-task1.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main() {
+    val studentOne: Byte = 3
+    val studentTwo: Byte = 4
+    val studentThree: Byte = 3
+    val studentFour: Byte = 5
+
+    val averageGrade: Float = (studentOne + studentTwo + studentThree + studentFour) / 4.toFloat()
+
+
+    println("$averageGrade")
+}

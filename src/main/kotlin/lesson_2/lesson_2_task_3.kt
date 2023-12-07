99b0cf7b1f17c0916e58a6f74350fd81e750295a
Rail_Minyaev <blastrm@gmail.com>
2023-12-07 19:43:04 +0500

    KS-2-3 Train time arrival

---
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -0,0 +1,16 @@
+package lesson_2
+
+fun main() {
+    val hourDeparture: Byte = 9
+    val minuteDeparture: Byte = 39
+    val travelTime: Short = 457
+    val oneHourInMinute: Byte = 60
+
+    val arrivalTimeInMinute: Short = (hourDeparture * oneHourInMinute + minuteDeparture + travelTime).toShort()
+    val hourArrival: Byte = (arrivalTimeInMinute / oneHourInMinute).toByte()
+    val minuteArrival: Byte = (arrivalTimeInMinute - hourArrival * oneHourInMinute).toByte()
+
+
+    println("%02d:%02d".format(hourArrival, minuteArrival))
+
+}

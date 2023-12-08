f6127c651b898492061bbd9982dace59be12054f
Rail_Minyaev <blastrm@gmail.com>
2023-12-09 00:52:35 +0500

    KS-3-1 Two greetings

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,9 @@
+package lesson_3
+
+fun main() {
+    val name: String = "Реактивный Енот"
+    var greetings: String = "Добрый день,"
+    println("$greetings $name")
+    greetings = "Добрый вечер,"
+    println("$greetings $name")
+}

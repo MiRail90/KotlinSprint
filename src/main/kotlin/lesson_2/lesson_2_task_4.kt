ca71e6bed0ce43a423494208e28b0a7738ccc976
Rail_Minyaev <blastrm@gmail.com>
2023-12-08 23:29:03 +0500

    KS-2-4 Percent of crystal and iron Ore

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -1,12 +1,13 @@
 package lesson_2
 
 fun main() {
-    val buffPercentInNumber: Float = 0.2F //20 процентов выраженное в числе
+    val buffPercent: Byte = 20
+    val coefficient: Byte = 100
     val crystalOre = 7
     val ironOre = 11
 
-    val countCrystalOreAfterBuff: Float = (crystalOre * buffPercentInNumber).toFloat()
-    val countIronOreAfterBuff: Float = (ironOre * buffPercentInNumber).toFloat()
+    val countCrystalOreAfterBuff: Float = (crystalOre * buffPercent/coefficient).toFloat()
+    val countIronOreAfterBuff: Float = (ironOre * buffPercent/coefficient).toFloat()
 
     println(
         """

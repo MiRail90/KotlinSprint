c3976401354fb489a163e06d55bb281eaf1a267e
Rail_Minyaev <blastrm@gmail.com>
2023-12-09 21:06:56 +0500

    KS-4-3 changed name added const Ship sailing

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -6,12 +6,12 @@ fun main() {
     val airHumidity: Int = 20
     val season: String = "зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${sunny == IDEAL_SUNNY && tent == IDEAL_TENT && airHumidity == IDEAL_AIR_HUMIDITY && season != NOT_IDEAL_SEASON}")
+    println("Благоприятные ли условия сейчас для роста бобовых? ${sunny == IS_IDEAL_SUNNY && tent == IDEAL_TENT && airHumidity == IDEAL_AIR_HUMIDITY && season != NOT_IDEAL_SEASON}")
 
 
 }
 
-const val IDEAL_SUNNY: Boolean = true
+const val IS_IDEAL_SUNNY: Boolean = true
 const val IDEAL_TENT: Boolean = true
 const val IDEAL_AIR_HUMIDITY: Int = 20
 const val NOT_IDEAL_SEASON: String = "зима"

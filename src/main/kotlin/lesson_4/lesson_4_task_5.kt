bd3402cf423b71fe13dd0d51ee15ebaebdcf05f9
Rail_Minyaev <blastrm@gmail.com>
2023-12-09 21:30:44 +0500

    KS-4-5 rename of val

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -15,9 +15,9 @@ fun main() {
     val countCrewMaxIdeal: Int = 70
     val countBoxesIdeal: Int = 50
 
-    val CountCrewSecondChance = 70
+    val countCrewSecondChance = 70
     val isFavorableWeatherConditionsSecondChance: Boolean = true
     val countBoxesSecondChance: Int = 50
 
-    println("Корабль готов к плаванию: ${((isDamagedShip == isDamagedShipIdeal) && (countCrew in (countCrewMinIdeal..countCrewMaxIdeal)) && (countBoxes > countBoxesIdeal)) or ((isDamagedShip != isDamagedShipIdeal) && (countCrew == CountCrewSecondChance) && (isFavorableWeatherConditions == isFavorableWeatherConditionsSecondChance) && (countBoxes >= countBoxesSecondChance))}")
+    println("Корабль готов к плаванию: ${((isDamagedShip == isDamagedShipIdeal) && (countCrew in (countCrewMinIdeal..countCrewMaxIdeal)) && (countBoxes > countBoxesIdeal)) or ((isDamagedShip != isDamagedShipIdeal) && (countCrew == countCrewSecondChance) && (isFavorableWeatherConditions == isFavorableWeatherConditionsSecondChance) && (countBoxes >= countBoxesSecondChance))}")
 }

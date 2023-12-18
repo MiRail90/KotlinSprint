078cb3f784de207e2a8580534bdbad6135fb73f5
Rail_Minyaev <blastrm@gmail.com>
2023-12-18 10:58:00 +0500

    KS-5-3 Упрощена логика Lottery two number

--- src/main/kotlin/lesson_5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -3,7 +3,6 @@ package lesson_5
 fun main() {
     val firstWinNumber = 5
     val secondWinNumber = 32
-    var giftLevel: String = ""
 
     println("Введите первое число (от 0 до 42):")
     val playerFirstNumber = readln().toInt()
@@ -11,18 +10,12 @@ fun main() {
     println("Введите второе число (от 0 до 42):")
     val playerSecondNumber = readln().toInt()
 
-
-
-    if ((playerFirstNumber == firstWinNumber || playerFirstNumber == secondWinNumber) && (playerSecondNumber == firstWinNumber || playerSecondNumber == secondWinNumber) && (((firstWinNumber == secondWinNumber) && (playerFirstNumber == playerSecondNumber)) || ((firstWinNumber != secondWinNumber) && (playerFirstNumber != playerSecondNumber))))  //вся сложность у меня возникла: если организатором были задуманы разные числа а игроком были введены два одинаковых числа - поэтому такой длинный код у меня получился
-        giftLevel = "Поздравляем! Вы выиграли главный приз!"
+    if ((playerFirstNumber == firstWinNumber && playerSecondNumber == secondWinNumber) || (playerFirstNumber == secondWinNumber && playerSecondNumber == firstWinNumber))
+        println("Поздравляем! Вы выиграли главный приз!")
     else if (playerFirstNumber == firstWinNumber || playerFirstNumber == secondWinNumber || playerSecondNumber == firstWinNumber || playerSecondNumber == secondWinNumber)
-        giftLevel = "Вы выиграли утешительный приз!"
-    else giftLevel = "Неудача!"
+        println("Вы выиграли утешительный приз!")
+    else println("Неудача!")
 
-
-
-
-    println(giftLevel)
     println("Победные числа $firstWinNumber и $secondWinNumber")
 
 }

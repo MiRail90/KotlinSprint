d66e2f82c9a7c3b173a65f931497dd9187142ec1
Rail_Minyaev <blastrm@gmail.com>
2024-01-29 22:11:38 +0500

    KS-8-5 enter Ingredients without FOR

--- src/main/kotlin/lesson_8/lesson_8_task_4.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_4.kt
@@ -2,20 +2,13 @@ package lesson_8
 
 fun main() {
     val ingredientsArray = arrayOf("перец", "соль", "мука", "вода")
-    for (i in ingredientsArray) println(i)
+    ingredientsArray.forEach { println(it) }
     println("Введите ингридиент, который хотите заменить:")
     val ingredientFromUser = readln()
     if (ingredientFromUser in ingredientsArray) {
         println("Введите ингридиент, который хотите добавить:")
-        val changeIngredientFromUser = readln()
-        for (i in ingredientsArray) {
-            if (i == ingredientFromUser) ingredientsArray[ingredientsArray.indexOf(i)] = changeIngredientFromUser
-        }
-        println("Готово! Вы сохранили следующий список: ")
-        for (i in ingredientsArray) {
-            println(i)
-        }
-    } else
-        println("Такого ингредиента в рецепте нет")
+        ingredientsArray[ingredientsArray.indexOf(ingredientFromUser)] = readln()
+        println("Готово! Вы сохранили следующий список: ${ingredientsArray.contentToString()}")
+    } else println("Вашего ингридиента нет в рецепте")
 }
 
